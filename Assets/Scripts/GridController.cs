bd2191f0e9d7bdc224cfc54a693c5aaf88bacce0
Kycklingris <malte.linde.neveling@gmail.com>
2022-11-18 14:37:02 +0100

    Segments flipping on mouse click

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -7,16 +7,24 @@ public class GridController : MonoBehaviour
     [SerializeField] private int height = 20;
     [SerializeField] private int width = 20;
     [SerializeField] private SegmentFlip segmentGameObject; 
+    [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private int segmentSize = 2;
+    [SerializeField] private int obstacleChance = 25;
 
-    [SerializeField] private Material regular;
+    [SerializeField] private Material regularMaterial;
+    [SerializeField] private Material targetMaterial;
+    [SerializeField] private Material pathMaterial;
 
 
-    private List<Segment> segments = new List<Segment>();   
+    private List<Segment> segments = new List<Segment>();
 
-    // Start is called before the first frame update
-    void Awake()
+    private Transform player;
+
+    private void Awake()
     {
+        this.player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        var playerPos = this.ObjectGridPosition(this.player);
+
         var startX = (this.width / 2) * this.segmentSize;
         var startZ = (this.height / 2) * this.segmentSize;
 
@@ -29,21 +37,60 @@ public class GridController : MonoBehaviour
             {
                 SegmentFlip instance = Instantiate(this.segmentGameObject, new Vector3(x, 0.0f, z), Quaternion.Euler(-90, 0, 0), this.gameObject.transform);
 
-                instance.SwitchMaterial(this.regular);
+                instance.SwitchMaterial(this.regularMaterial);
 
-                this.segments.Add(new Segment() {flip = instance});
+                var hasObstacle = false;
+                if (Random.Range(0, 101) <= this.obstacleChance && playerPos != new Vector2(x, z)) {
+                    GameObject obstacleInstance = Instantiate(this.obstaclePrefab, Vector3.zero, Quaternion.identity, instance.gameObject.transform);
+                    obstacleInstance.transform.localPosition = new Vector3(0.0f, 0.0f, 0.01f);
+                    hasObstacle = true;
+                }
+
+                this.segments.Add(new Segment() {flip = instance, hasObstacle = hasObstacle});
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000))
+            {
+                this.PathFind(this.ObjectGridPosition(hit.transform));
+            }   
+        }
+    }
+
+    private void PathFind(Vector2 segmentPosition)
+    {
+        var x = (int)Mathf.Abs(segmentPosition.x - this.width / 2);
+        var z = (int)Mathf.Abs(segmentPosition.y - this.height / 2);
+
+        var target = this.segments[(x * (this.height + 1)) + z];
+
+        if (target.hasObstacle)
+        {
+            return;
+        }
+
+        target.flip.FlipToMaterial(this.targetMaterial);
+    }
+
+    Vector2 ObjectGridPosition(Transform obj) 
+    {
+        return new Vector2(
+            Mathf.Floor(obj.position.x / this.segmentSize),
+            Mathf.Floor(obj.position.z / this.segmentSize)
+        );
     }
 }
 
 
-public class Segment {
+public class Segment 
+{
     public SegmentFlip flip; 
+    public bool hasObstacle;
 }
--- Assets/Scripts/SegmentFlip.cs
+++ Assets/Scripts/SegmentFlip.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SegmentFlip : MonoBehaviour
 {
     [SerializeField] private float flipTime = 0.5f;
-    [SerializeField] private float intervals = 0.001f;
+    [SerializeField] private float timeStep = 0.001f;
 
     private bool topUp = true;
     private MeshRenderer meshRenderer;
@@ -49,36 +49,29 @@ public class SegmentFlip : MonoBehaviour
 
     IEnumerator Flip() 
     {
-        var timeLeft = this.flipTime;
+        var intervals = this.flipTime / this.timeStep;
 
-        var degreeSteps = 180.0f / (this.flipTime / this.intervals);
+        var degreeSteps = 180.0f / intervals;
 
         var currentDegrees = this.gameObject.transform.localEulerAngles.x;
 
-        while (true)
+        for (int i = 0; i < intervals; i++)
         {
-            timeLeft -= this.intervals;
-
             currentDegrees += degreeSteps;
-
             this.gameObject.transform.localRotation = Quaternion.Euler(currentDegrees, 0, 0);
-
-            if (timeLeft <= 0)
-            {
-                this.topUp = !this.topUp;
-
-                if (this.topUp)
-                {
-                    this.gameObject.transform.localRotation = Quaternion.Euler(-90, 0, 0);
-                } else
-                {
-                    this.gameObject.transform.localRotation = Quaternion.Euler(90, 0, 0);
-                }
-                
-                yield break;
-            }
-
-            yield return new WaitForSeconds(this.intervals);
+            yield return new WaitForSeconds(this.timeStep);
         }
+
+        this.topUp = !this.topUp;
+
+        if (this.topUp)
+        {
+            this.gameObject.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        } else
+        {
+            this.gameObject.transform.localRotation = Quaternion.Euler(90, 0, 0);
+        }
+        
+        yield break;
     }
 }
